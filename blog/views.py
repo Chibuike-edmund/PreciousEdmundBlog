1346005e62ca31e3b830df8e1f79e474dc9fa8e6
Precious Edmund <Chidera1915@gmail.com>
2023-09-29 17:21:48 +0000

    update

--- blog/views.py
+++ blog/views.py
@@ -3,6 +3,15 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+posts = [
+    {
+        "about": "Precious Edmund",
+        "title": "Blog Post 1",
+        "Content": "First Post Content",
+        "Date_Posted": "September 29, 2023 "
+    },
+]
+
 def home(request):
     return render(request, "blog/home.html")
 
